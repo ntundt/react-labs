f1fa7cdace250d0a1d975e13e8f13db4656c7152
Skilsar <skilsar@gmail.com>
2021-04-09 16:29:46 +0300

    Материалы  для "Справочник" + Новые стили + Единый стиль списков
    1. Добавлены новые материалы в справочник.
    2. Новые стили для списков  внутри материалов.
    3. Единый стиль для списков: Лабораторные работы, Содержание, Теория.

--- libraries/header.js
+++ libraries/header.js
@@ -1,19 +1,18 @@
-
-(function($) { 
-  $(function() { 
-    $('nav ul li a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
-      $('.dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
+(function ($) {
+    $(function () {
+        $('nav ul li a:not(:only-child)').click(function (e) {
+            $(this).siblings('.nav-dropdown').toggle();
+            $('.dropdown').not($(this).siblings()).hide();
+            e.stopPropagation();
+        });
+        $('html').click(function () {
+            $('.nav-dropdown').hide();
+        });
+        $('#nav-toggle').click(function () {
+            $('nav ul').slideToggle();
+        });
+        $('#nav-toggle').on('click', function () {
+            this.classList.toggle('active');
+        });
     });
-    $('html').click(function() {
-      $('.nav-dropdown').hide();
-    });
-    $('#nav-toggle').click(function() {
-      $('nav ul').slideToggle();
-    });
-    $('#nav-toggle').on('click', function() {
-      this.classList.toggle('active');
-    });
-  }); 
 })(jQuery);
